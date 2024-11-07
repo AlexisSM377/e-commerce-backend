64c99ef9d90c377c096b98315b0900556ce2b18c
AlexisSM377 <al222111377@gmail.com>
2024-11-07 10:14:28 -0600

    feat: db migration

--- config/plugins.js
+++ config/plugins.js
@@ -1,1 +1,11 @@
-module.exports = () => ({});
+module.exports = {
+  'strapi-neon-tech-db-branches': {
+    enabled: true,
+    config: {
+      neonApiKey: "812tr6np6gci0ks85hh8mjwgl4rb9tnl7qcleq97miahgwekngafeeo868uo2l77",
+      neonProjectName: "ecommerce-isa",
+      neonRole: "neondb_owner",
+      gitBranch: "main"
+    }
+  },
+};
