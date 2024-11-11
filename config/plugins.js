3297e6de134c2bd0bd65bf519aafee138a9ee8b6
AlexisSM377 <al222111377@gmail.com>
2024-11-11 10:53:18 -0600

    feat: configure PostgreSQL connection settings in plugins.js

--- config/plugins.js
+++ config/plugins.js
@@ -1,1 +1,17 @@
-module.exports = () => ({});
+module.exports = ({ env }) => ({
+    connection: {
+      client: 'postgres',
+      connection: {
+        host: env('DATABASE_HOST', 'database-ecommers.cvoogg2sw6bq.us-east-1.rds.amazonaws.com'),
+        port: env.int('DATABASE_PORT', 5432),
+        database: env('DATABASE_NAME', 'postgres'),
+        user: env('DATABASE_USERNAME', 'postgres'),
+        password: env('DATABASE_PASSWORD', 'Q9XM9m)#88'),
+        ssl: {
+          rejectUnauthorized: false, // Habilita SSL si AWS lo requiere
+        },
+      },
+      debug: false,
+    },
+  });
+  
